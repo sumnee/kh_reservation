af10e222965c197732e8a9f6ab23401923a5c119
user1 <user1@DESKTOP-MPUKNTA>
2022-12-07 14:33:37 +0900

    Add reservation function
    예약자 이름을 입력해야 현황이 보이도록 수정

--- src/com/kh/lsm/VineFunction.java
+++ src/com/kh/lsm/VineFunction.java
@@ -28,16 +28,16 @@ public class VineFunction {
 		Scanner sc = new Scanner(System.in);
 		System.out.println();
 		System.out.println("~~~< 예약자 정보 입력 >~~~");
-		System.out.print("이름 : ");
+		System.out.print("이름 >>");
 		String name = sc.next();
-		System.out.print("연락처 : ");
+		System.out.print("연락처 >>");
 		String number = sc.next();
-		System.out.print("인원 : [ ]명");
+		System.out.print("인원 : [ ]명 >>");
 		int member = sc.nextInt();
-		System.out.println("날짜 (yyyymmdd 형식으로 입력) : ");
+		System.out.println("날짜 (yyyymmdd 형식으로 입력) >> ");
 		int day = sc.nextInt();
 		System.out.println("이용 시간은 1부 17~19시 / 2부 19시~21시 중 선택 가능합니다.");
-		System.out.print("원하는 시간대 : [ ]부");
+		System.out.print("원하는 시간대 : [ ]부 >>");
 		int time = sc.nextInt();
 		System.out.println();
 		System.out.println("VineWine에 예약해주셔서 감사합니다.");
@@ -47,19 +47,29 @@ public class VineFunction {
 		viner = new Vine (name, number, member, day, time);	
 	}
 	public void check() {
+		Scanner sc = new Scanner(System.in);
 		Random ran = new Random();
 		int com = ran.nextInt(2);
 		System.out.println();
 		System.out.println("~~~< 예약 정보 확인 >~~~");
-		System.out.println(viner.getName() + "님의 예약 내역입니다.");
-		if(com == 1) {
-			System.out.println("상태 : 예약이 확정되었습니다!");
+		System.out.print("예약자 이름을 입력해주세요 >> ");
+		String reserName = sc.next();
+		if(reserName.equals(viner.getName())) {
+			System.out.println();
+			System.out.println(viner.getName() + "님의 예약 내역입니다.");
+			System.out.println();
+			if(com == 1) {
+				System.out.println("상태 : 예약이 확정되었습니다!");
+			} else {
+				System.out.println("상태 : 가게에서 예약 가능 여부를 확인 중입니다.");
+			}
+			System.out.println("연락처 : " + viner.getNumber());
+			System.out.println("예약 인원 : " + viner.getMember() + "명");
+			System.out.println("예약 일시 : " + viner.getDay() + " / " + viner.getTime() + "부");
 		} else {
-			System.out.println("상태 : 가게에서 예약 가능 여부를 확인 중입니다.");
+			System.out.println();
+			System.out.println("입력하신 이름으로 예약이 없습니다.");
 		}
-		System.out.println("연락처 : " + viner.getNumber());
-		System.out.println("예약 인원 : " + viner.getMember() + "명");
-		System.out.println("예약 일시 : " + viner.getDay() + " / " + viner.getTime() + "부");
 	}
 
 	public void bestMenu() {
